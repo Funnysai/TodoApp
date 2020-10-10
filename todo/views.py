4869355fb69354c5f7199cfe730cf4fe636152b5
Funnysai <funnysai01@gmail.com>
2020-10-10 09:24:16 +0530

    Initial Commit.

---
+++ todo/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt as csrf
+from django.utils import timezone
+from todo.models import Todo
+#from django.http import HttpResponseRedirect
+
+# Create your views here.
+def home(request):
+    todo_items = Todo.objects.all().order_by('added_date')
+    return render(request, 'todo/index.html', {'todo_items': todo_items})
+
+@csrf
+def add_todo(request):
+    current_date = timezone.now()
+    content = request.POST['content']
+    created_obj = Todo.objects.create(added_date=current_date,text=content)
+    lengt_of_todos = Todo.objects.all().count()
+    return redirect('/')
+
+@csrf
+def delete_todo(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return redirect('/')
